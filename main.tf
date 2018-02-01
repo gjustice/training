017420e86f1059bb6d79395159eff5addab31fdd
gjustice <35980799+gjustice@users.noreply.github.com>
2018-01-31 16:25:23 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  count                  = "3"
+  count                  = "2"
   ami                    = "ami-66506c1c"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-ce92e593"
